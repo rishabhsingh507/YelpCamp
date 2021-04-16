d30bd21e44c6a07bf74913d7b337254cecdbb08b
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-04-16 23:13:35 +0530

    style: Added flash and partials

--- routes/reviews.js
+++ routes/reviews.js
@@ -26,12 +26,14 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     campground.reviews.push(review)
     await review.save()
     await campground.save()
+    req.flash('success', 'Created New Review')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.delete('/:reviewId', catchAsync(async (req, res, next) => {
     const { id, reviewId } = req.params
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    req.flash('success', 'Successfully deleted review')
     await Review.findByIdAndDelete(reviewId);
     res.redirect(`/campgrounds/${id}`)
 }))
