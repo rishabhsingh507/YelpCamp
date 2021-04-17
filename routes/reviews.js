4a5700529bc5de42628469c393c86c16d90c3923
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-04-18 00:05:54 +0530

    feat: added authorization for campgrounds and reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const Campground = require('../models/campground')
 const Review = require('../models/review')
 
@@ -9,20 +9,10 @@ const { reviewSchema } = require('../schemas.js')
 const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else {
-        next();
-    }
-}
-
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
+    const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review)
     await review.save()
     await campground.save()
@@ -30,7 +20,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:reviewId', catchAsync(async (req, res, next) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res, next) => {
     const { id, reviewId } = req.params
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     req.flash('success', 'Successfully deleted review')
