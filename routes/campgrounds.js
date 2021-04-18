e01c89538e4744c7c81dd88346bd106f8effe755
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-04-18 16:29:50 +0530

    feat: Added star ratings

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const campgrounds = require('../controllers/campgrounds')
-const catchAsync = require('../utils/catchAsync')
-const ExpressError = require('../utils/ExpressError')
-const Campground = require('../models/campground')
+const campgrounds = require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
 
-const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
-router.get('/', catchAsync(campgrounds.index))
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampgrounds))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampgrounds))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampgrounds));
 
-router.get('/:id', catchAsync(campgrounds.showCampgrounds))
-
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampgrounds))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampgrounds))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 module.exports = router;
 
--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,16 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
-const users = require('../controllers/users')
+const users = require('../controllers/users');
+const { route } = require('./campgrounds');
 
-router.get('/register', users.renderRegister);
-router.post('/register', catchAsync(users.register));
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout', users.logout)
 
