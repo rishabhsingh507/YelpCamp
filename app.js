7bc1b13d23ea1dba56f8e08b66420ee15a56f72e
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-04-20 00:06:36 +0530

    fix: Minor bug

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
